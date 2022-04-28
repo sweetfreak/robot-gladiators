20ffbfd079e15ff85a2c01bd188bf9eec358b9f2
jesse sheehan <jessetsheehan@gmail.com>
2022-04-27 21:52:06 -0400

    added preliminary js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Javascript... is runnin'! ");
+// function fight(){
+//     window.alert("The fight... has begun!");
+// }
+var name = "your name";
+console.log(name);
+
+
+var playerName = window.prompt("That is your robot's name?");
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a mess")
+
+// fight();
