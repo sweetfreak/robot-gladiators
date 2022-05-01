191e81b7bdb514e3f1d15ecccefc0f28c6334400
jesse sheehan <jessetsheehan@gmail.com>
2022-04-30 22:55:49 -0400

    start and endgame functions added

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,9 +57,54 @@ var fight = function(enemyName) {
     //alert players that they're starting the round.
         // window.alert("Welcome to Robooooot Glaaaadiatooorrs!");
 //execute function:
-for (var i=0; i < enemyNames.length; i++){
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    //call fight function with enemy-robot
-    fight(enemyNames[i]);
+
+var startGame = function () {    
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i=0; i < enemyNames.length; i++){
+        if (playerHealth > 0) {
+            //alert players that they're starting the round.
+            window.alert("Welcome to Robooooot Glaaaadiatooorrs! Round " + (i + 1) + "... BEGIN!");
+    
+            var pickedEnemyName = enemyNames[i];
+            //reset enemy health
+            enemyHealth = 50;
+
+            //debugger;
+
+            //call fight function with enemy-robot
+            fight(pickedEnemyName); 
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over man!");
+            break;
+        }
+    }
+    //play again
+    startGame();
 }
+   
+var endGame = function() {
+    var playAgainConfirm =  window.confirm("Wanna go another round?");
+
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    } else {
+        window.alert("Thanks for player Robot Gladiator. Come back soon ya hear?")
+    }
+    //if player is still alive, player wins
+    if (playerHealth > 0) {
+        window.alert("Great job, ya made it out alive. And your payout - err - your score is " + playerMoney + "!");
+    }
+    window.alert("You've lose your robot in battle. Pour one out for em.");
+}
+
+startGame();
+
+
+
+
